4f8e383c5f71b4c38dd8b338178fa88d5f593a73
Esteban Salomon <esalomon6492@southwestuniversity.edu>
2021-05-14 21:31:51 -0600

    Module 1 Activity

---
+++ Module1/Module1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 10;
+            int y = 8;
+            int z = 0;
+
+            z = x + y;
+            Console.WriteLine(z);
+        }
+    }
+}
